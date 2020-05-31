e2b45fe0df6ed9bf948711cd7fd54f94404de1d5
Soura, Masato <marzdog0319@gmail.com>
2020-05-31 23:14:52 +0900

    update

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+  
+DOTPATH=~/dotfiles
+TARBALL="https://github.com/marzdog0319/dotfiles/archive/master.tar.gz"
+
+curl -L $TARBALL | tar xvz
+if [ ! -d dotfiles-master ]; then
+  echo "dotfiles-master: not found"
+  exit 1
+fi
+
+mv dotfiles-master $DOTPATH
